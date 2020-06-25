23aedb1e2eeb60446c0478d2bd8b192962bcfa66
Marki <marek.labuzik@gmail.com>
2020-06-25 10:30:24 +0200

    Create Package.swift
    test

---
+++ Package.swift
@@ -0,0 +1,35 @@
+import PackageDescription
+
+let package = Package(
+    name: "youtube-ios-player-helper",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "youtube-ios-player-helper", targets: ["youtube-ios-player-helper"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MarkyAli/ocmock.git", .upToNextMajor(from: "0.0.0"))
+    ],
+    targets: [
+        .target(name: "Moya", dependencies: ["Alamofire"]),
+    ]
+)
+
+#if canImport(PackageConfig)
+import PackageConfig
+
+let config = PackageConfiguration([
+    "rocket": [
+	"before": [
+            
+	],
+	"after": [
+            
+	]
+    ]
+]).write()
+#endif
